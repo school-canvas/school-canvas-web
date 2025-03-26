45891d8e62aab2d39efe5ccff5d9248c32ee161c
Prakash Kafle <kafleprakash96@gmail.com>
2025-03-26 15:17:50 -0500

    Added header footer skeleton

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { AuthModule } from '../../features/auth/auth.module';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [SharedModule,HeaderComponent,FooterComponent,RouterModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })

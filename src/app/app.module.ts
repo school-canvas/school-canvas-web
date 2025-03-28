f6e2feb808c3931a49abb48f60f4204317677290
Prakash Kafle <kafleprakash96@gmail.com>
2025-03-28 17:09:46 -0500

    Removed app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
-import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { CoreModule } from './core/core.module'
 import { LayoutsModule } from './layouts/layouts.module'
@@ -19,7 +17,6 @@ import { routes } from './app.routes'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
     CoreModule,
     LayoutsModule,
     SharedModule,

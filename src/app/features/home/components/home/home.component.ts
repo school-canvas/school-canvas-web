b381a05e4c16958de3e8882a4a414b771ebbae9a
Prakash Kafle <kafleprakash96@gmail.com>
2025-03-28 21:02:53 -0500

    Added home component implementation

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApplicationConfig } from '../../../../../application-config';
+import { SharedModule } from '../../../../shared/shared.module';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  appConfig = ApplicationConfig;
+  safeMapUrl!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+      const mapUrl = this.appConfig.mapEmbedUrl;
+      this.safeMapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(mapUrl);
+
+  }
+
 
 }
---
+++ src/app/shared/pipes/safe-resource-url.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safeResourceUrl',
+  standalone: false
+})
+export class SafeResourceUrlPipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+
+  transform(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}

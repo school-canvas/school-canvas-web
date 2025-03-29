f8a39f44d0e8420ddd08cae23130c009e2b998ca
Prakash Kafle <kafleprakash96@gmail.com>
2025-03-28 20:45:23 -0500

    Added footer implementation

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { ApplicationConfig } from '../../../../application-config'
+import { SharedModule } from '../../shared.module'
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  appConfig = ApplicationConfig
+  currentYear = new Date().getFullYear()
 }

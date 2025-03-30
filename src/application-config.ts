5e9ece5ff870918bbe9986f2d57312a2e2cedfcf
Prakash Kafle <kafleprakash96@gmail.com>
2025-03-30 15:25:05 -0500

    Updated correct map

--- src/application-config.ts
+++ src/application-config.ts
@@ -24,7 +24,7 @@ export const ApplicationConfig = {
   },
 
   // Location settings
-  mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.1234567890123!2d85.32412345678901!3d27.71724599999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1923456789ab%3A0x123456789abcdef0!2sNawal%20English%20Boarding%20School!5e0!3m2!1sen!2snp!4v1234567890123',
+  mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7067.038203045414!2d84.18380981345467!3d27.670346587156452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399456d334ca6287%3A0x18bb346d2b452819!2sNew%20Nawal%20English%20Boarding%20School!5e0!3m2!1sen!2sus!4v1743365973867!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade',
 
 
   // Footer links
